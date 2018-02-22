c16c785b9813bf06b2ae4d4f6c3b59b6e194b105
alexander sydorenko <dangerzone@alexanders-MacBook-Pro.local>
2018-02-22 10:31:27 -0700

    add checkboxes and list items

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import ToDo from './components/ToDo.js'
 
 class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       todos: [
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ]
+     };
+   }
+
   render() {
     return (
       <div className="App">
         <ul>
-        <ToDo />
-        <ToDo />
+          {this.state.todos.map( (todo, index) => 
+            <ToDo key={ index } description={todo.description} isCompleted={todo.isCompleted} />
+          )}
        </ul>
       </div>
     );
